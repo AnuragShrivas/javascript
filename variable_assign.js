eebf4551536b227e68366fc1ccbdc15c04ee55d4
AnuragShrivas <Anurag.a.shrivas@gmail.com>
2023-05-06 07:28:41 +0530

    first js code done

---
+++ variable_assign.js
@@ -0,0 +1,49 @@
+console.log("undefined");
+
+ 
+
+var fullName = "Anurag Anil Shrivas";
+
+console.log(fullName);
+
+var name = "I LOVE ONLY JS";
+
+console.log(name);
+
+var firstName = "Anurag ";
+
+console.log(firstName);
+
+var lastName = "Shrivas";
+
+console.log(lastName);
+
+
+
+ 
+
+var age; // Variable Declaration 
+
+age = "26";  // Variable Initialization
+
+console.log(age);
+
+ 
+var address = "39 z.p Teachers colony,Rathi Nagar , Amravati";
+
+console.log(address);
+
+var pincode = "444603";
+
+console.log(pincode);
+
+var gender = "Male";
+
+console.log(gender);
+
+var isMarried = "NO";
+
+console.log(isMarried);
+
+
+
